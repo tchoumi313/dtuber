85096645426fd97c0fb5f70d51564a69d3e8ee99
tchoumi313 <75470956+tchoumi313@users.noreply.github.com>
2023-03-22 01:45:40 +0100

    new api

--- src/utils/DownloadWithApi.js
+++ src/utils/DownloadWithApi.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "https://youtube-video-download-info.p.rapidapi.com/dl";
+const BASE_URL = "https://yt-api.p.rapidapi.com/dl";
+//"https://youtube-video-stream-download.p.rapidapi.com/api/v1/Youtube/getVideoUrls";
 
-const options = {
+/* const options = {
   //method: "GET",
   //url: 'https://youtube-video-download-info.p.rapidapi.com/dl',
   //params: { id: "UxxajLWwzqY" },
@@ -10,15 +11,25 @@ const options = {
     "X-RapidAPI-Key": "c8565a1d17msh0f92ae73e1605b4p158570jsnb9a220ae652a",
     "X-RapidAPI-Host": "youtube-video-download-info.p.rapidapi.com",
   },
+}; */
+
+const options = {
+  //method: 'GET',
+  //url: 'https://youtube-video-stream-download.p.rapidapi.com/api/v1/Youtube/getVideoUrls/oWUVQ1Nq7FQ',
+  headers: {
+    "X-RapidAPI-Key": "70ac684616msh1226d54380229a3p1921d6jsnb6752204aa51", //"c8565a1d17msh0f92ae73e1605b4p158570jsnb9a220ae652a",
+    "X-RapidAPI-Host": "yt-api.p.rapidapi.com", //"youtube-video-stream-download.p.rapidapi.com",
+  },
 };
 
 export const DownloadWithAPI = async (id) => {
   // console.log(id);
   // const navigate = useNavigate();
-  const { data } = await axios.get(`${BASE_URL}?id=${id}`, options);
+  //const { data } = await axios.get(`${BASE_URL}/?id=${id}`, options);
   //console.log(data?.link?.[243][0]);
+  const { data } = await axios.get(`${BASE_URL}?id=${id}`, options);
   //navigate(`${data?.link?.[243][0]}`);
-  const link = data?.link?.[243][0];
-
-  return link; //JSON.stringify(link, null, 2);
+  //const link = data?.urls?.videos?.[2].url;
+  // console.log(link);
+  return data?.formats[2].url; //JSON.stringify(link, null, 2);
 };
--- src/utils/FetchFromAPI.js
+++ src/utils/FetchFromAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 const options = {
-  params: { maxResults: "50" },
+  params: { maxResults: "2" },
   headers: {
     "X-RapidAPI-Key": "70ac684616msh1226d54380229a3p1921d6jsnb6752204aa51", //"c8565a1d17msh0f92ae73e1605b4p158570jsnb9a220ae652a", //process.env.REACT_APP_RAPID_API_KEY, //"70ac684616msh1226d54380229a3p1921d6jsnb6752204aa51",
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
